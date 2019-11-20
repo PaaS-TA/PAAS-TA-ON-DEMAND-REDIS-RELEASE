7bd19357333c3cab7003af55b38f9b7aa6423176
parkcheolhan <35785850+parkcheolhan@users.noreply.github.com>
2019-11-20 11:09:11 +0900

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
-bosh create-release --force --tarball paas-ta-on-demand-redis-release.tgz --name paas-ta-on-demand-redis-release --version 1.0
+bosh create-release --force --tarball paasta-on-demand-redis-release.tgz --name paasta-on-demand-redis-release --version 1.0
 
-bosh upload-release paas-ta-on-demand-redis-release.tgz
+bosh upload-release paasta-on-demand-redis-release.tgz
