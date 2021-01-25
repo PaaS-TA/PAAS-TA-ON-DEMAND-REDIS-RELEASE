73bf873758a5dae6dbe61bc23d13582e1a1b6aef
이문희 <moonii@users.noreply.github.com>
2021-01-25 17:40:30 +0900

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,38 @@
-bosh create-release --force --tarball paasta-on-demand-redis-release.tgz --name paasta-on-demand-redis-release --version 1.0
 
-bosh upload-release paasta-on-demand-redis-release.tgz
+if [ -z $1 ]; then
+        echo "//=========================
+//
+//   release name is empty 
+//
+//=========================
+"
+        return
+else
+    echo "//=========================
+//
+//   release name is $1
+//
+//=========================
+"
+fi
+
+if [ -z $2 ]; then 
+        echo "//=========================
+//
+//   version is empty 
+//
+//=========================
+"
+	return           
+else
+    echo "//=========================
+//
+//   version is $2
+//
+//=========================
+"
+fi
+
+bosh create-release --force --tarball ${1}-release-${2}.tgz --name ${1} --version ${2}
+
+bosh upload-release ${1}-release-${2}.tgz
